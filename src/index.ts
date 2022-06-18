b6c87b7d0843bdd8b03296cc288e6ff3f1fedace
Jobayerdev <jobayerhossain977@gmail.com>
2022-06-18 22:57:25 +0600

    main: Added

---
+++ src/index.ts
@@ -0,0 +1,80 @@
+/ import { BehaviorSubject, Observable } from 'rxjs';
+const LocalStorageService = {
+  set: (key: string, value: any): void => {
+    localStorage.setItem(key, value);
+  },
+  get: (key: string): string | null => {
+    return localStorage.getItem(key);
+  },
+  delete: (key: string): void => {
+    localStorage.removeItem(key);
+  },
+  clearAll: (): void => {
+    localStorage.clear();
+  },
+};
+const CacheStore = {
+  get: (key: string) => {
+    try {
+      return JSON.parse(LocalStorageService.get(key) as any);
+    } catch (error) {
+      return null;
+    }
+  },
+  set: (key: string, value: any) => {
+    return LocalStorageService.set(key, JSON.stringify(value));
+  },
+  remove: (key: string) => {
+    return LocalStorageService.delete(key);
+  },
+};
+
+import { BehaviorSubject, Observable } from 'rxjs'
+export class BaseStore<T> {
+  private obs$!: BehaviorSubject<T>;
+  private cacheKey: string | undefined;
+  private initialValueSnapShot: T;
+  constructor(initialValue: T, cacheKey?: string) {
+    this.cacheKey = cacheKey;
+    if (isEmpty(initialValue)) {
+      this.obs$ = new BehaviorSubject<T>(CacheStore.get(cacheKey as string));
+    } else {
+      this.obs$ = new BehaviorSubject<T>(initialValue);
+    }
+    this.initialValueSnapShot = initialValue;
+  }
+  getValue = (): T => this.obs$.value;
+  get = (): Observable<T> => this.obs$.asObservable();
+  set = (payload: T) => {
+    this.cacheKey ? CacheStore.set(this.cacheKey, payload) : null;
+    this.obs$.next(payload);
+  };
+  update = (callBack: (pv: T) => T) => {
+    const updatedValues = callBack(this.getValue());
+    this.cacheKey ? CacheStore.set(this.cacheKey, updatedValues) : null;
+    this.obs$.next(updatedValues);
+  };
+  patch = (payload: Partial<T>) => {
+    let margeValues = { ...this.obs$.value, ...payload };
+    this.cacheKey ? CacheStore.set(this.cacheKey, margeValues) : null;
+    this.obs$.next(margeValues);
+  };
+  reset = () => {
+    this.cacheKey ? CacheStore.remove(this.cacheKey) : null;
+    this.obs$.next(this.initialValueSnapShot);
+  };
+  clear = () => {
+    this.obs$.next(null as any);
+    this.cacheKey ? CacheStore.remove(this.cacheKey) : null;
+  };
+}
+
+const isEmpty = (value: any): boolean => {
+  if (typeof value === 'object') {
+    return Object.keys(value).length === 0;
+  }
+  if (Array.isArray(value)) {
+    return value.length === 0;
+  }
+  return value === undefined || value === null || value === '';
+};
